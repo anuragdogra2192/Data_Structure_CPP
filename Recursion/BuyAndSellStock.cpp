fed1663be912618d5d970f87c136b306df96ec2b
Anurag Dogra <anuragdogra.2192@gmail.com>
2025-06-10 01:23:34 +0200

    Leet Code 121. Best Time to Buy and Sell Stock - Solved

---
+++ Recursion/BuyAndSellStock.cpp
@@ -0,0 +1,69 @@
+/* Leet Code: 121 
+121. Best Time to Buy and Sell Stock
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+Constraints:
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void getMaxProfit(vector<int>& prices, int minPrice, int& maxProfit, int i)
+    {
+        //base case
+        if(i == prices.size()) return;
+
+        //1k case mein karunga
+        int todayPrice = prices[i];
+
+        if(todayPrice < minPrice)
+        {
+            minPrice = todayPrice;
+        }
+        int todayProfit = todayPrice - minPrice;
+
+        if(todayProfit > maxProfit)
+        {
+            maxProfit = todayProfit;
+        }
+
+        //baaki recursion sambhalega
+        getMaxProfit(prices, minPrice, maxProfit, i+1);
+
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+        getMaxProfit(prices, minPrice, maxProfit, 0);
+        return maxProfit;
+    }
+};

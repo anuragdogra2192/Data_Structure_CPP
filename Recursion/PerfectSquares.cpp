bf08dce608d83549cd5abf27eb24c199f2399e7c
Anurag Dogra <anuragdogra.2192@gmail.com>
2025-06-15 04:49:00 +0200

    Added the time complexity and Space complexity

--- Recursion/PerfectSquares.cpp
+++ Recursion/PerfectSquares.cpp
@@ -1,14 +1,17 @@
 /* Leet Code 279 
 279. Perfect Squares
-Given an integer n, return the least number of perfect square numbers that sum to n.
+Given an integer n, return the least number of perfect square numbers
+that sum to n.
 
-A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+A perfect square is an integer that is the square of an integer;
+in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 
 Example 1:
 
 Input: n = 12
 Output: 3
 Explanation: 12 = 4 + 4 + 4.
+
 Example 2:
 
 Input: n = 13
@@ -18,6 +21,10 @@ Explanation: 13 = 4 + 9.
 Constraints:
 
 1 <= n <= 104
+Time Complexity O(sqrt(n)^n) ~ O(n^n)
+Space Complexity - O(n)
+
+TIME Limit Exceeded, we can optimize it with Dyanmic Programming.
 */
 
 #include<iostream>
